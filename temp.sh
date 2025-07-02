feaba81aea9ab09f2807b49ad8309104fd5608f4
Anton Putilov <58368713+aputilov-perm@users.noreply.github.com>
2025-07-02 21:46:30 +0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,11 +1,28 @@
-#!/bin/bash 
-echo 'Updating repo' apt update
-echo 'Removing unuse packages' 
-apt autoremove - y
-echo 'Installing basic software'
-packages='mc htop iotop net- tools jq'
-for package in $package
-do 
-  echo 'Installing' $package
-  apt install $package - y done
-echo 'All packages have been installed'
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <city>"
+  exit 1
+fi
+
+CITY=$1
+OUTPUT_FILE="/var/www/html/index.html"
+
+WEATHER_DATA=$(curl -s "wttr.in/${CITY}?format=j1")
+TEMP=$(echo "$WEATHER_DATA" | jq '.current_condition[0].temp_C')
+HUMIDITY=$(echo "$WEATHER_DATA" | jq '.current_condition[0].humidity')
+
+cat <<EOF > "$OUTPUT_FILE"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Weather in $CITY</title>
+</head>
+<body>
+    <h1>Current Weather in $CITY</h1>
+    <p>Temperature: $TEMP °C</p>
+    <p>Humidity: $HUMIDITY %</p>
+</body>
+</html>
+EOF
